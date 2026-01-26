e8ee87d43b6808adc2ce6e63535d01f0ee05a23b
KIMMINKI <rlaalrl00@gmail.com>
2026-01-26 18:14:59 +0900

    project_setting_final

---
+++ 20260121_full_adder/sim/tb_adder.v
@@ -0,0 +1,64 @@
+`timescale 1ns / 1ps
+
+module tb_adder();
+
+    // tb_adder local variable
+    reg a, b, cin;
+    wire sum, carry;
+
+// instanciate half adder
+// half_adder dut (
+//     .a(a),
+//     .b(b),
+//     .sum(sum),
+//     .carry(carry)
+// );
+
+// instanciate full adder
+full_adder dut (
+    .a(a),
+    .b(b),
+    .cin(cin),
+    .sum(sum),
+    .c(carry)
+);
+
+initial begin
+    #0;
+    a = 0;
+    b = 0;
+    cin = 0;
+    #10;
+    a = 1;
+    b = 0;
+    cin = 0;
+    #10;
+    a = 0;
+    b = 1;
+    cin = 0;
+    #10;
+    a = 1;
+    b = 1;
+    cin = 0;
+    #10;
+    a = 0;
+    b = 0;
+    cin = 1;
+    #10;
+    a = 1;
+    b = 0;
+    cin = 1;
+    #10;
+    a = 0;
+    b = 1;
+    cin = 1;
+    #10;
+    a = 1;
+    b = 1;
+    cin = 1;
+    #10;
+    $stop;
+    #100;
+    $finish;
+end
+endmodule
